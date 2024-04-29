228a3235fb035ba92352243d1057bfbeafb2f89c
Kshilinaa <ksusha_2605@mail.ru>
2024-04-29 18:40:40 +0300

    Завершена верстка экрана

--- SUI_DZ/SUI_DZ/ContentView.swift
+++ SUI_DZ/SUI_DZ/ContentView.swift
@@ -1,26 +1,84 @@
 //
 //  ContentView.swift
 //  SUI_DZ
-//
-//  Created by Ксения Шилина on 29.04.2024.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack  {
+            Text("Alert & ActionSheet")
+                .font(Font.custom("Inter", size: 20))
+                .fontWeight(.bold)
+                .padding(30)
+            
+            HStack(spacing: 45) {
+                VStack(alignment: .leading, spacing: 25) {
+                    Text("Пример Alert")
+                        .frame(width: 150, height: 48, alignment: .leading)
+                        .font(.headline.bold())
+                    Text("Пример Alert с 2\n кнопками логикой")
+                        .frame(width: 160, height: 48, alignment: .leading)
+                        .font(.headline.bold())
+                    Text("Пример ActionSheet")
+                        .frame(width: 150, height: 48, alignment: .leading)
+                        .font(.headline.bold())
+                    Text("Пример ActionSheet кнопками и логикой")
+                        .frame(width: 169, height: 48, alignment: .leading)
+                        .font(.headline.bold())
+                    Spacer()
+                }
+                
+                
+                VStack(spacing: 25) {
+                    Button(action: {
+                        print("Показать Alert 1")
+                    }) {
+                        Text("Показать")
+                            .frame(width: 150, height: 48)
+                            .background(Color("PurpleColor"))
+                            .foregroundColor(.white)
+                            .cornerRadius(12)
+                    }
+                    
+                    Button(action: {
+                        print("Показать Alert 2")
+                    }) {
+                        Text("Показать")
+                            .frame(width: 150, height: 48)
+                            .background(Color("PurpleColor"))
+                            .foregroundColor(.white)
+                            .cornerRadius(12)
+                    }
+                    
+                    Button(action: {
+                        print("Показать ActionSheet 1")
+                    }) {
+                        Text("Показать")
+                            .frame(width: 150, height: 48)
+                            .background(Color("PurpleColor"))
+                            .foregroundColor(.white)
+                            .cornerRadius(12)
+                    }
+                    
+                    Button(action: {
+                        print("Показать ActionSheet 2")
+                    }) {
+                        Text("Показать")
+                            .frame(width: 150, height: 48)
+                            .background(Color("PurpleColor"))
+                            .foregroundColor(.white)
+                            .cornerRadius(12)
+                    }
+                    Spacer()
+                }
+            }
+            .padding(30)
         }
-        .padding()
     }
-}
-
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
