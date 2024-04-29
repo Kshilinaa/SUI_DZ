43bdbb411340e712fc08272f75f69dc9f0e27107
Kshilinaa <ksusha_2605@mail.ru>
2024-04-29 14:47:46 +0300

    init

---
+++ SUI_DZ/SUI_DZ/SUI_DZApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUI_DZApp.swift
+//  SUI_DZ
+//
+//  Created by Ксения Шилина on 29.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SUI_DZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
